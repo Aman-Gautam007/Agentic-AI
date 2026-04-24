041b2ba95f9a4be27c75fcad3e2a3fb17a8add50
Aman Gautam <amangautam2002@gmail.com>
2026-04-25 01:07:25 +0530

    Data Types in Python

---
+++ Data-Types.py
@@ -0,0 +1,21 @@
+string_var = "Hello World"  # string
+integer_var = 50  # integer
+float_var = 60.5  # float
+complex_var = 3j  # complex
+list_var = ["geeks", "for", "geeks"]  # list
+tuple_var = ("geeks", "for", "geeks")  # tuple
+dict_var = {"name": "Suraj", "age": 24}  # dict
+set_var = {"geeks", "for", "geeks"}  # set
+bool_var = True  # bool
+binary_var = b"Geeks"  # binary
+
+print("String:", string_var)
+print("Integer:", integer_var)
+print("Float:", float_var)
+print("Complex:", complex_var)
+print("List:", list_var)
+print("Tuple:", tuple_var)
+print("Dict:", dict_var)
+print("Set:", set_var)
+print("Boolean:", bool_var)
+print("Binary:", binary_var)
